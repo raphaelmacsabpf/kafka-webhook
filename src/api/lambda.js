2120b534b9f5e44b6d0479940ec8f03a0d1ac3ee
Raphael Santos <raphaelmacsa@gmail.com>
2020-03-01 23:08:05 -0300

    fix: correct controller method invocation

--- src/api/lambda.js
+++ src/api/lambda.js
@@ -7,7 +7,7 @@ const app = express();
 const router = express.Router();
 app.use(bodyParser.json());
 
-router.post('/webhook', webhookController.post);
+router.post('/webhook', webhookController.post.bind(webhookController));
 
 app.use(router);
 
